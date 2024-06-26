3ec1ddf2481ea54744162ad520039bbf86ca9589
Đào Duy Thái <thaiddhe176315@fpt.edu.vn>
2024-06-26 21:26:32 +0700

    Update OrderDetailDAO.java

--- ECommerce/src/java/dal/OrderDetailDAO.java
+++ ECommerce/src/java/dal/OrderDetailDAO.java
@@ -11,10 +11,44 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import model.OrderDetail;
+
 /**
  *
  * @author DUYTHAI
  */
 public class OrderDetailDAO {
-    
+
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<OrderDetail> getAllOrderDetails() {
+        List<OrderDetail> list = new ArrayList<>();
+        String sql = "SELECT * FROM ORDER_DETAILS";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new OrderDetail(
+                        rs.getInt("id"),
+                        rs.getInt("product_id"),
+                        rs.getInt("order_id"),
+                        rs.getInt("quantity"),
+                        rs.getLong("price")
+                ));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        OrderDetailDAO dao = new OrderDetailDAO();
+        List<OrderDetail> list = dao.getAllOrderDetails();
+        for (OrderDetail p : list) {
+            System.out.println(p.toString());
+        }
+    }
 }
