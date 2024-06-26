966c33e4915d1b9dadfc6bde22c1268ab4893efd
Đào Duy Thái <thaiddhe176315@fpt.edu.vn>
2024-06-26 21:23:52 +0700

    test orderDAO

--- ECommerce/src/java/dal/OrderDAO.java
+++ ECommerce/src/java/dal/OrderDAO.java
@@ -16,5 +16,38 @@ import model.Order;
  * @author DUYTHAI
  */
 public class OrderDAO {
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Order> getAllOrders() {
+        List<Order> list = new ArrayList<>();
+        String sql = "SELECT * FROM ORDERS";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Order(
+                        rs.getInt("id"),
+                        rs.getInt("account_id"),
+                        rs.getLong("total_price"),
+                        rs.getString("delivery_address"),
+                        rs.getString("status"),
+                        rs.getString("note")
+                ));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return list;
+    }
     
+    public static void main(String[] args) {
+        OrderDAO dao = new OrderDAO();
+        List<Order> list = dao.getAllOrders();
+        for(Order p : list){
+            System.out.println(p.toString());
+        }
+    }
 }
