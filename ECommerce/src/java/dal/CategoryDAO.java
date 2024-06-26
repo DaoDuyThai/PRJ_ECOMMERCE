5de05a57bc7758144901ff97521bd061a34e3b3e
Đào Duy Thái <thaiddhe176315@fpt.edu.vn>
2024-06-26 21:21:19 +0700

    test categories dao

--- ECommerce/src/java/dal/CategoryDAO.java
+++ ECommerce/src/java/dal/CategoryDAO.java
@@ -4,10 +4,47 @@
  */
 package dal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Category;
 /**
  *
  * @author DUYTHAI
  */
 public class CategoryDAO {
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Category> getAllCategories() {
+        List<Category> list = new ArrayList<>();
+        String sql = "SELECT * FROM CATEGORIES";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Category(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("description")
+                ));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return list;
+    }
     
+    public static void main(String[] args) {
+        CategoryDAO dao = new CategoryDAO();
+        List<Category> list = dao.getAllCategories();
+        for(Category p : list){
+            System.out.println(p.toString());
+        }
+    }
 }
--- ECommerce/src/java/dal/OrderDetailDAO.java
+++ ECommerce/src/java/dal/OrderDetailDAO.java
@@ -4,6 +4,13 @@
  */
 package dal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.OrderDetail;
 /**
  *
  * @author DUYTHAI
