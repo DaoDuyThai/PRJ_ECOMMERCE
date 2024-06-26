cd3344cb6b7e92396c3f9892554bc31a9d30d29c
Đào Duy Thái <thaiddhe176315@fpt.edu.vn>
2024-06-26 21:19:31 +0700

    add DAOs files

---
+++ ECommerce/src/java/dal/AccountDAO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Account;
+
+/**
+ *
+ * @author DUYTHAI
+ */
+public class AccountDAO {
+    
+}
