a269f7901ae570d7613cb6d8b924a3abdd7b088e
Đào Duy Thái - HE176315 <daoduythai.business@gmail.com>
2024-06-26 10:56:55 +0700

    Create Account.java

---
+++ ECommerce/src/java/model/Account.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Account {
+    int id;
+    String email;
+    String password;
+    String fullname;
+    String avatar_url;
+    String role;
+    
+    public Account() {
+    }
+
+    public Account(int id, String email, String password, String fullname, String avatar_url, String role) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.fullname = fullname;
+        this.avatar_url = avatar_url;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" + "id=" + id + ", email=" + email + ", password=" + password + ", fullname=" + fullname + ", avatar_url=" + avatar_url + ", role=" + role + '}';
+    }
+
+}
