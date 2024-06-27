3e9fc2d830e7b051379adea2e7f78ae10811bc27
Đào Duy Thái <thaiddhe176315@fpt.edu.vn>
2024-06-27 21:35:43 +0700

    bug fixing for css and js not being rendered

--- ECommerce/src/java/controller/IndexController.java
+++ ECommerce/src/java/controller/IndexController.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
+
 package controller;
 
 import java.io.IOException;
@@ -14,41 +15,38 @@ import jakarta.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author Lenovo
+ * @author DUYTHAI
  */
-@WebServlet(name = "indexController", urlPatterns = {"/"})
+@WebServlet(name="IndexController", urlPatterns={"/index"})
 public class IndexController extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet indexController</title>");            
+            out.println("<title>Servlet IndexController</title>");  
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet indexController at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet IndexController at " + request.getContextPath () + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    }
+    } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
+    /** 
      * Handles the HTTP <code>GET</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -56,13 +54,12 @@ public class IndexController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        request.getRequestDispatcher("index.").forward(request, response);
-    }
+    throws ServletException, IOException {
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    } 
 
-    /**
+    /** 
      * Handles the HTTP <code>POST</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -70,13 +67,12 @@ public class IndexController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
+    /** 
      * Returns a short description of the servlet.
-     *
      * @return a String containing servlet description
      */
     @Override
